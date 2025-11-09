2c8386b6a4d845e8ffc5750e56f569d70a0db6cc
Vlad_the_programmer <vladyslavklymchuk.com>
2025-11-09 19:32:21 +0100

    Clean users/usrl.py urls to look nicer with no extra symbols in the url route like in dj-rest-auth/urls.py ones. Change re_path() into path().

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import  path, re_path
+from django.urls import  path
 
 from rest_framework_simplejwt.views import TokenVerifyView
 
@@ -15,12 +15,13 @@ app_name = 'users'
 
 urlpatterns = [
     path('', RegisterView.as_view(), name='rest_register'),
-    re_path(r'login/?$', LoginView.as_view(), name='rest_login'),
+    path('login/', LoginView.as_view(), name='rest_login'),
+
     # URLs that require a user to be logged in with a valid session / token.
-    re_path(r'logout/?$', LogoutView.as_view(), name='rest_logout'),
-    re_path(r'user/?$', UserDetailsView.as_view(), name='rest_user_details'),
-    re_path(r'token/verify/?$', TokenVerifyView.as_view(), name='token_verify'),
-    re_path(r'token/refresh/?$', get_refresh_view().as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='rest_logout'),
+    path('user/', UserDetailsView.as_view(), name='rest_user_details'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
 
     path('api/users/<int:id>/delete/', UserDestroyView.as_view(), name='user-delete'),
 
